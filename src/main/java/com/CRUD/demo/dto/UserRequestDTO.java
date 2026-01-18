21d96b0f3e48bc1ef95421822cc66c619fed33fd
PratikC54 <pratiksenapati54@gmail.com>
2026-01-18 23:33:18 +0530

    Added validation in user request.

--- src/main/java/com/CRUD/demo/dto/UserRequestDTO.java
+++ src/main/java/com/CRUD/demo/dto/UserRequestDTO.java
@@ -1,7 +1,17 @@
 package com.CRUD.demo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class UserRequestDTO {
+
+    @NotBlank(message = "Name must not be empty")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 ")
     private String name;
+
+    @NotBlank(message = "Email must not be empty")
+    @Email(message = "Email must be valid")
     private String email;
 
     public UserRequestDTO() {}
