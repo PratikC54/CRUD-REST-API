73d5f25506ea25aa7b447b8268d02e58d70dd292
PratikC54 <pratiksenapati54@gmail.com>
2026-01-24 21:15:29 +0530

    Added Pagination and sorting

--- src/main/java/com/CRUD/demo/repositories/UserRepository.java
+++ src/main/java/com/CRUD/demo/repositories/UserRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.*;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByEmail(String email);
