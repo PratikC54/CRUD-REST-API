47599f5e811e38179dd3b853fc8e536de19408e1
PratikC54 <pratiksenapati54@gmail.com>
2026-01-25 23:05:08 +0530

    Added Swagger for clean UI.

--- src/main/java/com/CRUD/demo/DemoApplication.java
+++ src/main/java/com/CRUD/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.CRUD.demo;
 
-import  org.springframework.boot.SpringApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "User management API",
+				version = "1.0",
+				description = "Backend API for managing users"
+		)
+)
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
