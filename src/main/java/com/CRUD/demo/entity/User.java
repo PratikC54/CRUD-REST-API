d14ab783a25f783bd586cfb0d5aa18983eb42bbc
PratikC54 <pratiksenapati54@gmail.com>
2026-01-26 20:18:58 +0530

    Added password to DB.

--- src/main/java/com/CRUD/demo/entity/User.java
+++ src/main/java/com/CRUD/demo/entity/User.java
@@ -21,7 +21,18 @@ public class User {
     @Column(nullable = false , unique = true)
     private String email ;
 
+    @Column(nullable = false)
+    private String password;
+
     public User(){}
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 
     public int getId() {
         return id;
